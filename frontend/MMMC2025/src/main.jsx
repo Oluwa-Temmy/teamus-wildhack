2da8400af023c81c3beafbeff3f91c2937c3ae3b
apaolaoliveira <paolaoliveira.dev@gmail.com>
2025-04-06 03:05:16 -0500

    add: landing page and cta

--- frontend/MMMC2025/src/main.jsx
+++ frontend/MMMC2025/src/main.jsx
@@ -3,17 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react'
+import GlobalStyles from './styles/global.js'
+import theme from './styles/theme.js'
+import { ThemeProvider } from 'styled-components'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Auth0Provider
-      domain="dev-5n7c70icgitjmxpc.us.auth0.com"
-      clientId="ni6YPJZDWl4WBQFvb6MtkNJhFC4tm3Wn"
-      authorizationParams={{
-        redirect_uri: window.location.origin + '/dashboard',
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <ThemeProvider theme={theme}>
+      <Auth0Provider
+        domain="dev-5n7c70icgitjmxpc.us.auth0.com"
+        clientId="ni6YPJZDWl4WBQFvb6MtkNJhFC4tm3Wn"
+        authorizationParams={{
+          redirect_uri: window.location.origin + '/dashboard',
+        }}
+        >
+        <GlobalStyles />
+        <App />
+      </Auth0Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ frontend/MMMC2025/src/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#1c2231',
+    GOLD: '#e8b763',
+    DARKBLUE: '#192032',
+    WHITE: '#faf6f3',
+    LIGHTGRAY: '#c4c1bf',
+    LIGHTBLUE: '#6f91ed',
+  }
+}
